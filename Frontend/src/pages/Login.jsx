ccc73f16a39d5dfc0cad3df71dc716a196c569aa
Aanjaneya Dikhit <dikhitaanjaneyat03@gmail.com>
2026-07-21 12:14:23 +0530

    third commit

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -266,7 +266,7 @@ function RegisterView({ onSwitch }) {
     setApiError("");
     
     try {
-      const response = await axios.post("http://localhost:5000/api/auth/register", {
+      const response = await axios.post("https://realityone-epr.onrender.com/api/auth/register", {
         name: name,
         email: email,
         password: password,
